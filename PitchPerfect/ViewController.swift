c0815f22a2b33ddba735a88b15b4cd7bccd0e9d5
KassioVieira <kassio.vieira7@gmail.com>
2021-01-18 20:24:12 -0300

    start project Pitch Perfect with Swift

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Kássio Luz on 18/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stop recording button was pressed")
+    }
+}
+
